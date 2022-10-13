da0502a9fcc85b6c4c2bf32a82bfcc4a5e4a1d4f
nnnnadia <tristaonadia@gmail.com>
2022-10-13 15:09:35 -0300

    shell scripting 1: 2.11

--- Alura/shell-scripting-1/conversao-jpg-png.sh
+++ Alura/shell-scripting-1/conversao-jpg-png.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+convert_image(){
 cd ~/scripts/Alura/shell-scripting-1/imagens-livros
 if [ ! -d png ]
 then
@@ -10,5 +11,11 @@ do
 	image_wo_extension=$(ls $imagem | awk -F. '{ print $1 }')
 	convert $image_wo_extension.jpg png/$image_wo_extension.png
 done
-
-
+}
+convert_image
+if [ $? -eq 0 ]
+then
+	echo "Conversão realizada com sucesso"
+else
+	echo "Houve uma falha no processo de conversão"
+fi
