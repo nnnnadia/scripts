65035a9d2bc4bb5402397d1ca4306aa63a5ec6d4
nnnnadia <tristaonadia@gmail.com>
2022-10-17 13:47:46 -0300

    shell scripting 2: 1.6

--- Alura/shell-scripting-2/filtrando-resultado-apache.sh
+++ Alura/shell-scripting-2/filtrando-resultado-apache.sh
@@ -7,6 +7,10 @@ regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 if [[ $1 =~ $regex ]]
 then
 	cat apache.log | grep $1
+	if [ $? -ne 0 ]
+	then
+		echo "O endereço IP procurado não está presenteno arquivo"
+	fi
 else
 	echo "Formato não é válido"
 fi
