353b2a241900a5803f4611ab25e614ca01d99f43
nnnnadia <tristaonadia@gmail.com>
2022-10-17 16:11:36 -0300

    shell scripting 2: 1.13

--- Alura/shell-scripting-2/filtro-requisicao.sh
+++ Alura/shell-scripting-2/filtro-requisicao.sh
@@ -2,16 +2,37 @@
 
 cd ~/scripts/Alura/shell-scripting-2/apache-log
 
-if [ $1 == "GET" ]
+if [ -z $1 ]
 then
-	cat apache.log | grep GET
-elif [ $1 == "POST" ]
-then
-	cat apache.log | grep POST
-elif [ $1 == "PUT" ]
-then
-	cat apache.log | grep PUT
-elif [ $1 == "DELETE" ]
-then
-	cat apache.log | grep DELETE
+	while [ -z $requisicao ]
+	do
+		read -p "Você esqueceu de colocar o parâmetro (GET, PUT, POST, DELETE): " requisicao
+		letra_maiuscula=$(echo $requisicao | awk '{ print toupper($1) }')
+	done
+else
+	letra_maiuscula=$(echo $1 | awk '{ print toupper($1) }')
 fi
+
+case $letra_maiuscula in
+	GET)
+	cat apache.log | grep GET
+	;;
+
+	POST)
+	cat apache.log | grep POST
+	;;
+
+	PUT)
+	cat apache.log | grep PUT
+	;;
+
+	DELETE)
+	cat apache.log | grep DELETE
+	;;
+
+	*)
+	echo "O parâmetro passado não é válido"
+	;;
+esac
+
+
