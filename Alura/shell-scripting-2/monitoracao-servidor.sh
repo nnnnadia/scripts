e49649cd7e8d49bb71fc2cd1706c1747b67cc550
nnnnadia <tristaonadia@gmail.com>
2022-10-18 15:28:45 -0300

    shell scripting 2: 2.8

--- Alura/shell-scripting-2/monitoracao-servidor.sh
+++ Alura/shell-scripting-2/monitoracao-servidor.sh
@@ -2,4 +2,11 @@
 
 resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost/mutillidae.html)
 
-echo $resposta_http
+if [ $resposta_http -eq 200 ]
+then
+	echo "Tudo está ok com o servidor"
+else
+	echo "Houve um problema no servidor. Tentando reinicializar..."
+	systemctl restart apache2
+	echo "Servidor reinicializado"
+fi
